d6b1ade51b8a4bd2e7f1302f9216d6e067155201
karthik <karthik007310@gmail.com>
2022-12-06 20:46:47 -0500

    1st

---
+++ src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container">
+    <HeaderTop bookName="Workout Plan" authorName="Beginner" />
+
+    <BookBox :books="books" />
+
+</div>
+</template>
+
+<script>
+
+import HeaderTop from './components/HeaderTop.vue'
+import BookBox from './components/BookBox.vue'
+
+// import axios from "axios";
+export default {
+  name: 'App',
+  components: {    
+    HeaderTop,
+    BookBox
+  },
+
+  data(){
+    return {
+      books: []
+    }
+
+  },
+
+  methods: {
+
+    // promises
+
+    async fetchBooks(){
+      const res = await fetch('https://agreeable-gray-ox.cyclic.app/api')
+      const data = await res.json()
+      console.log( data )
+      return data
+    }
+
+
+  },
+  async created(){
+    this.books = await this.fetchBooks()
+  }
+}
+  // mounted() {
+  //   axios
+  //       .get("https://karthikfinalproject.herokuapp.com/api")
+  //       .then((res) => {
+  //         console.log(res);
+  //         this.books = res.data;
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  // },
+
+  // methods: {
+  //
+  //   // promises
+  //
+  //     async fetchBooks(){
+  //       const url='https://karthikfinalproject.herokuapp.com/api';
+  //       return new Promise((resolve, request)=>{
+  //         try {
+  //
+  //           fetch(url).then((res)=> {return res.json()}).then((res)=>{
+  //             resolve(
+  //                 res.map(pos=>({
+  //                   ...pos
+  //                 }))
+  //             )
+  //           });
+  //         }catch (err){
+  //           request(err);
+  //         }
+  //       })
+  //     }
+  //
+  //
+  // },
+  // async created(){
+  //   this.books = await this.fetchBooks()
+  //   console.log(this.books);
+  // }
+
+
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 0.5em;
+}
+
+</style>
---
+++ src/components/BookItem.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <div>
+        <h1> {{book.day}}</h1>
+        <h2> {{book.category}}</h2>
+        <section id="nameof"><h4> Type of workout : {{book.typeofworkout}}</h4></section>
+        <h4> Difficulty : {{book.difficulty}}</h4>
+        <h4> Sets : {{book.sets}}</h4>
+        <h4> Reps : {{book.reps}}</h4>
+        <h4> Rest Time : {{book.resttime}}</h4>
+        
+    </div>
+
+</template>
+
+<script>
+
+
+
+
+export default{
+    name: 'BookItem',
+    props:{
+        book: Array
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+h2{
+    text-align: center;
+}
+h4{
+    margin: 2px;
+}
+h1{
+    text-align: center;
+}
+#nameof{
+    background-color: #b1ff9e;
+    color: black;
+    padding: 1em;
+
+}
+div{
+    background-color: #ffb89e;
+    color: black;
+    padding: 1em;
+}
+
+.red{
+background-color: orangered !important;
+
+}
+.green{
+    color: white;
+}
+
+
+</style>
